c35e023fcb0d2f70a12e40dc921213a54158dd89
Robert Vollmer <dev@robv.de>
2025-02-02 22:31:24 +0000

    wip

--- src/services/gps_service/gps_service.cpp
+++ src/services/gps_service/gps_service.cpp
@@ -1,5 +1,54 @@
-//
-// Created by clemens on 02.08.24.
-//
-
 #include "gps_service.hpp"
+
+#include <drivers/gps/nmea_gps_driver.h>
+#include <drivers/gps/ublox_gps_driver.h>
+
+void GpsService::OnCreate() {
+}
+
+bool GpsService::Configure() {
+  if (strncmp(Protocol.value, "UBX", Protocol.length) == 0) {
+    protocol_ = UBX;
+  } else if (strncmp(Protocol.value, "NMEA", Protocol.length) == 0) {
+    protocol_ = NMEA;
+  } else {
+    return false;
+  }
+  return true;
+}
+
+void GpsService::OnStart() {
+  using namespace xbot::driver::gps;
+  if (protocol_ == UBX) {
+    gps_driver_ = new UbxGpsDriver();
+  } else {
+    gps_driver_ = new NmeaGpsDriver();
+  }
+
+  gps_driver_->SetStateCallback(
+      etl::delegate<void(const GpsDriver::GpsState&)>::create<GpsService, &GpsService::GpsStateCallback>(*this));
+
+  // TODO: Make port configurable.
+  gps_driver_->StartDriver(&UARTD8, Baudrate.value);
+}
+
+void GpsService::OnStop() {
+  delete gps_driver_;
+}
+
+void GpsService::tick() {
+}
+
+bool GpsService::OnRTCMChanged(const uint8_t* new_value, uint32_t length) {
+  (void)new_value;
+  (void)length;
+  return false;
+}
+
+void GpsService::GpsStateCallback(const GpsDriver::GpsState& state) {
+  StartTransaction();
+  double position[3] = {state.pos_lat, state.pos_lon, state.pos_height};
+  SendPosition(position, 3);
+  SendPositionAccuracy(state.position_h_accuracy);
+  CommitTransaction();
+}
--- src/services/gps_service/gps_service.hpp
+++ src/services/gps_service/gps_service.hpp
@@ -1,10 +1,36 @@
-//
-// Created by clemens on 02.08.24.
-//
-
 #ifndef GPS_SERVICE_HPP
 #define GPS_SERVICE_HPP
 
-class GpsService {};
+#include <drivers/gps/gps_driver.h>
+
+#include "GpsServiceBase.hpp"
+
+using namespace xbot::driver::gps;
+
+class GpsService : public GpsServiceBase {
+ private:
+  THD_WORKING_AREA(wa, 1024) {};
+
+ public:
+  explicit GpsService(const uint16_t service_id) : GpsServiceBase(service_id, 10000, wa, sizeof(wa)) {
+  }
+
+ protected:
+  bool Configure() override;
+  void OnStart() override;
+  void OnStop() override;
+  void OnCreate() override;
+
+ private:
+  void tick() override;
+
+  enum ProtocolType { UBX, NMEA };
+  ProtocolType protocol_;
+  GpsDriver* gps_driver_;
+  void GpsStateCallback(const GpsDriver::GpsState& state);
+
+ protected:
+  bool OnRTCMChanged(const uint8_t* new_value, uint32_t length) override;
+};
 
 #endif  // GPS_SERVICE_HPP
