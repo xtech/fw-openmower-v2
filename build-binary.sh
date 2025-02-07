29238288fc5623f59700432dfd24523292ca110b
Clemens Elflein <clemens1@familie-elflein.de>
2025-02-07 18:00:00 +0100

    fixed build script

--- build-binary.sh
+++ build-binary.sh
@@ -2,12 +2,13 @@
 set -euo pipefail
 
 PRESET=${1:-Release}
+ROBOT=${2:-YardForce}
 
 mkdir -p build out
 
 cd build
-cmake .. --preset=$PRESET
+cmake .. --preset=$PRESET -DROBOT_PLATFORM=$ROBOT
 cd $PRESET
 make -j$(nproc)
 
-cp -v openmower-with-unsafe-bootloader.bin openmower.bin openmower.elf ../../out/
+cp -v openmower.elf ../../out/openmower-$ROBOT.elf
