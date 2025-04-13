b8623d3cc77f8ae954fe5a7e5d29011800e6263f
Robert Vollmer <dev@robv.de>
2025-04-13 23:19:32 +0000

    Initialize and call drivers from InputService

--- src/services/input_service/input_service.cpp
+++ src/services/input_service/input_service.cpp
@@ -1,8 +1,21 @@
 #include "input_service.hpp"
 
+#include <ulog.h>
+
 bool InputService::OnStart() {
+  // Start drivers.
+  for (auto& driver : drivers_) {
+    if (!driver.second->OnStart()) {
+      ULOG_ERROR("Failed to start input driver %d", driver.first);
+      return false;
+    }
+  }
   return true;
 }
 
 void InputService::tick() {
+  // TODO: Check if these can run at the same frequency.
+  for (auto& driver : drivers_) {
+    driver.second->tick();
+  }
 }
--- src/services/input_service/input_service.hpp
+++ src/services/input_service/input_service.hpp
@@ -1,8 +1,15 @@
 #ifndef INPUT_SERVICE_HPP
 #define INPUT_SERVICE_HPP
 
+#include <etl/flat_map.h>
+#include <etl/string.h>
+
 #include <InputServiceBase.hpp>
 
+#include "drivers/input/gpio_input_driver.hpp"
+#include "drivers/input/worx_input_driver.hpp"
+
+using namespace xbot::driver::input;
 using namespace xbot::service;
 
 class InputService : public InputServiceBase {
@@ -11,6 +18,14 @@ class InputService : public InputServiceBase {
   }
 
  private:
+  GpioInputDriver gpio_driver_;
+  WorxInputDriver worx_driver_;
+
+  const etl::flat_map<etl::string<4>, InputDriver*, 2> drivers_ = {
+      {"gpio", &gpio_driver_},
+      {"worx", &worx_driver_},
+  };
+
   bool OnStart() override;
   void tick();
   ManagedSchedule tick_schedule_{scheduler_, IsRunning(), 20'000,
