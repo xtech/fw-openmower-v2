9535f45c72e715b3c29425ddf1b2d3add0d193bb
Clemens Elflein <clemens1@familie-elflein.de>
2025-06-13 22:18:26 +0000

    Added HighlevelService
    Co-authored-by: Robert Vollmer <dev@robv.de>

--- src/services.hpp
+++ src/services.hpp
@@ -4,6 +4,7 @@
 #include "services/diff_drive_service/diff_drive_service.hpp"
 #include "services/emergency_service/emergency_service.hpp"
 #include "services/gps_service/gps_service.hpp"
+#include "services/high_level_service/high_level_service.hpp"
 #include "services/imu_service/imu_service.hpp"
 #include "services/input_service/input_service.hpp"
 #include "services/mower_service/mower_service.hpp"
@@ -16,6 +17,7 @@ extern ImuService imu_service;
 extern PowerService power_service;
 extern GpsService gps_service;
 extern InputService input_service;
+extern HighLevelService high_level_service;
 
 void StartServices();
 
