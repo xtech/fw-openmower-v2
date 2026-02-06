60944fe60c0899eb7e905cf0a71a10fb33f5ec55
Jörg Ebeling <joerg@ebeling.ws>
2026-02-06 13:02:05 +0100

    Add Universal7S robot platform

---
+++ robots/include/universal_7s_robot.hpp
@@ -0,0 +1,29 @@
+#ifndef UNIVERSAL_7S_ROBOT_HPP
+#define UNIVERSAL_7S_ROBOT_HPP
+
+#include "universal_robot.hpp"
+
+/**
+ * @class Universal7SRobot
+ * @brief Universal robot platform variant for 7S (7-series) lithium battery packs.
+ */
+class Universal7SRobot : public UniversalRobot {
+ public:
+  float Power_GetDefaultBatteryFullVoltage() override {
+    return 7.0f * 4.2f;
+  }
+
+  float Power_GetDefaultBatteryEmptyVoltage() override {
+    return 7.0f * 3.3f;
+  }
+
+  float Power_GetAbsoluteMinVoltage() override {
+    // 3.3V min, 7s pack
+    return 7.0f * 3.0;
+  }
+
+ private:
+  BQ2576 charger_{};
+};
+
+#endif  // UNIVERSAL_7S_ROBOT_HPP
